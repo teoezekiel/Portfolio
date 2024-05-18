025a186fa29eaa0bd6314d28ea8b5d707f6a8944
Teo Albert <112470991+teoezekiel@users.noreply.github.com>
2024-05-18 18:02:10 +0630

    made changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,8 +21,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> You</strong>
+                I'm
+                <strong className="main-name"> Kai Albert</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
